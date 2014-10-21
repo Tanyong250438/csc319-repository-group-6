ad5bce44dda0b422342b643d62bb4d4b45a53af6
Tanyong250438 <tanyong250438@gmail.com>
2014-10-21 23:22:55 +0700

    There are only the button that we need.

--- Sprint1/Controller.java
+++ Sprint1/Controller.java
@@ -6,9 +6,11 @@ public class Controller extends JFrame{
     JTextField text1 =new JTextField(20);
     JTextField text4 =new JTextField(20);
     public Controller(){
+        //set the frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(860, 430);
         setResizable(false);
+        //to arrange the frame
         setLayout(new GridLayout(1, 2));
         
         add(forest);
@@ -19,7 +21,7 @@ public class Controller extends JFrame{
         {
             JPanel controller1=new JPanel();
             controller.add(controller1);
-            { 
+            {   
                 stepButton=new JButton("Step");
                 stepButton.addActionListener(new ActionListener(){});
                 controller1.add(stepButton);
