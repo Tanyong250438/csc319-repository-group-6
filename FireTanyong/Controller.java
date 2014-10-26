a55f9f4f89dd1234e06cd7be60f729ad686c5e5f
Tanyong250438 <tanyonh250438@gmail.com>
2014-10-26 21:06:08 +0700

    Sprint 1

---
+++ FireTanyong/Controller.java
@@ -0,0 +1,153 @@
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * The controller class of project from MVC pattern
+ * 
+ * @author ....
+ * @version 2014.04.11
+ */
+public class Controller extends JFrame{
+    ModelClass myModel;
+    View myView;
+    Thread startThread;
+    JButton startButton,stopButton,moveButton;
+    JLabel probability,ratio,empty,size,delay;
+    JSlider probabilityScale,ratioScale,emptyScale,sizeScale,delayScale;
+    JTextField setProp =new JTextField(15);
+    JTextField setSize =new JTextField(15);
+    JTextField setHight =new JTextField(15);
+
+    /**
+     * Create the GUI of project
+     */
+    public Controller(){
+        //Create the main frame
+        super("Spread of Fire");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(860, 430);
+        setResizable(false);
+        setLayout(new GridLayout(1, 2));
+        
+        //Create the model, the main process of project
+        myModel=new ModelClass();
+        
+        //Create the view, the output of main process of project
+        int boxWidth=(int)((400)/myModel.width);
+        int boxHeight=(int)(400/myModel.height);
+        myView=new View(boxWidth,boxHeight);
+        
+        //Add the myView panel to the left
+        add(myView);
+        
+        //Let myView be the observer of myModel
+        myModel.addObserver(myView);
+        
+        //Create the controller panel
+        JPanel controller=new JPanel();
+        controller.setLayout(new GridLayout(4,1));
+        
+        //Add the controller panel to the right
+        add(controller);
+        
+        //Properties of controller panel
+        {
+            
+            JPanel controller2=new JPanel();
+            controller.add(controller2);
+            {
+                JLabel prop = new JLabel("PropCatch : ");
+                controller2.add(prop);
+                controller2.add(setProp);
+           
+            }
+            
+            
+            JPanel controller3=new JPanel();
+            controller.add(controller3);
+           
+            {
+                JLabel size = new JLabel("Set Size : ");
+                controller3.add(size);
+                controller3.add(setSize);
+            }
+            
+            
+            JPanel controller4=new JPanel();
+            controller.add(controller4);
+           
+            {
+                JLabel hight = new JLabel("Set Hight : ");
+                controller4.add(hight);
+                controller4.add(setHight);
+            }
+            
+            //Create and Add the 1st row to controller panel
+            JPanel controller1=new JPanel();
+            controller.add(controller1);
+            //Properties of 1st row
+            {
+                           
+                //Create and Add the startButton
+                startButton=new JButton("Auto");
+                startButton.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        //If there is no Thread or Thread is dead, create new Thread and start
+                        if(startThread==null||!startThread.isAlive()){
+                            startThread=new Thread() {  
+                                public void run() { 
+                                    myModel.checkBurn();
+                                }  
+                            };
+                            startThread.start();
+                        }
+                    }
+                });
+                controller1.add(startButton);
+                
+                //Create and Add the stopButton
+                stopButton=new JButton("Step");
+                stopButton.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        //If There is an alive Thread, stop it
+                        if(startThread!=null&&startThread.isAlive()){
+                            startThread.stop();
+                        }
+
+                    }
+                });
+                controller1.add(stopButton);
+                
+                //Create and Add the resetButton
+                JButton resetButton=new JButton("Reset");
+                resetButton.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        //If There is an alive Thread, stop it
+                        if(startThread!=null&&startThread.isAlive()){
+                            startThread.stop();
+                        }
+                        //Reset the main process
+                        myModel.fieldReset();
+                    }
+                });
+                controller1.add(resetButton);
+            }
+            
+            
+            
+            
+        }
+        //Set the frame Visible
+        setVisible(true);
+        
+    }
+
+}
---
+++ FireTanyong/ModelClass.java
@@ -0,0 +1,138 @@
+import java.util.*;
+import java.awt.Color;
+
+public final class ModelClass {
+   
+ public int width,height,probability;
+    private int green,red,yellow;
+    private View observer;
+    private Cell cell[][];
+    
+    public ModelClass() {
+        this(30,30,50);
+    }
+    public ModelClass(int width,int height){
+        this(width,height,50);
+    }
+    
+    public ModelClass(int width, int height, int similarity) {
+         //Set the properties of field
+        this.width = width;
+        this.height = height;
+        this.probability = probability;
+        observer=null;
+        //Reset the field
+        fieldReset();
+    }
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+    public void setSize(int width,int height){
+        this.width=width;
+        this.height=height;
+        //Reset the field after set
+        fieldReset();
+    }
+    public void fieldReset(){
+        //Create new field with current size
+        cell=new Cell[width][height];
+        for(int i = 0; i < cell.length; i++){
+            for(int j=0; j<cell[0].length; j++){
+                if(i==0 || j==0 || i==cell.length-1 || j==cell[0].length-1 ){
+                    cell[i][j] = new Cell(Cell.YELLOW);
+                }else{
+                    cell[i][j] = new Cell(Cell.GREEN);
+                }
+            }
+            
+        cell[width/2][height/2] = new Cell(Cell.RED);
+        }
+        
+        //Update the field
+        update();
+    }
+    public void print(){
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                if(cell[i][j].get()==Cell.GREEN)System.out.print(". ");
+                else if(cell[i][j].get()==Cell.RED)System.out.print("O ");
+                else if(cell[i][j].get()==Cell.YELLOW)System.out.print("  ");
+            }
+            System.out.println("");
+        }
+    }
+    public Color getColor(int x,int y){
+        try{
+            return cell[x][y].getColor();
+        }
+        catch(Exception e){
+            //If x,y out of bound, treat as empty
+            return Color.WHITE;
+        }
+    }
+    private int get(int x,int y){
+        try{
+            return cell[x][y].get();
+        }
+        catch(Exception e){
+            //If x,y out of bound, treat as empty
+            return Cell.YELLOW;
+        }
+    }
+    public void spread(int x, int y){
+        
+                
+        if(get(x,y-1) == Cell.GREEN){
+            int rnd = (int)(Math.random()*100);
+            if(rnd<probability){
+                cell[x][y-1].set(Cell.RED);
+        
+            }
+        }
+        if(get(x,y+1) == Cell.GREEN){
+            int rnd = (int)(Math.random()*100);
+            if(rnd<probability){
+                cell[x][y+1].set(Cell.RED);
+               
+            }      
+        }
+        if(get(x-1,y) == Cell.GREEN){
+            int rnd = (int)(Math.random()*100);
+            if(rnd<probability){
+                cell[x-1][y].set(Cell.RED);
+                
+            }      
+        }
+        if(get(x+1,y) == Cell.GREEN){
+            int rnd = (int)(Math.random()*100);
+            if(rnd<probability){
+                cell[x+1][y].set(Cell.RED);
+                
+            }      
+        }
+        
+        cell[x][y].set(Cell.YELLOW);
+    }
+    public void checkBurn(){
+        for(int k=0;k < cell.length;k++){
+           for(int i = 0; i < cell.length; i++){
+            for(int j=0; j<cell[0].length; j++){
+                if(get(i,j) == Cell.RED){
+                    spread(i,j);
+    
+                }
+            }
+          }
+        }
+        
+    }
+    public void addObserver(View view){
+        observer=view;
+        update();
+    }
+    public void update(){
+        if(observer!=null)observer.update(cell);    
+    }
+}
+
+    
