44302899d486d651c3016671f13a34df2bb0bb78
oilpanthasit <oilzayo22820@gmail.com>
2014-11-21 19:10:08 +0700

    sprint 4 code comment

---
+++ Sprint4/SpreadOfFire(comment)/src/spreadoffire/SpreadOfFire.java
@@ -0,0 +1,19 @@
+package spreadoffire;
+
+import javax.swing.JFrame;
+/**
+ * The main class of this project
+ * 
+ * @author KMUTT student 
+ * 56130500206
+ * 56130500221
+ * 56130500254
+ * @version 2.0
+ */
+public class SpreadOfFire{
+  public static void main(String[] args){     
+    JFrame myGUI=new Controller();
+  }
+  
+}
+
